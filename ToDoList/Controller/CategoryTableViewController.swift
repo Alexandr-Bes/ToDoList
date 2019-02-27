0f86948f1b1d3e0e2bdde312f7ac1f618e50f140
AlexBezkopylnyi <sanok.bes@gmail.com>
2019-02-27 22:33:07 +0200

    Add Category TableVC and new Entity for it

---
+++ ToDoList/Controller/CategoryTableViewController.swift
@@ -0,0 +1,44 @@
+//
+//  CategoryTableViewController.swift
+//  ToDoList
+//
+//  Created by Alexandr on 2/27/19.
+//  Copyright © 2019 Alex.Ltd. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryTableViewController: UITableViewController {
+
+
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    //MARK: - TableView Datasource Methods
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+    //MARK: - TableView Delegate Methods
+
+    //MARK: - Data Manipulation Methods
+
+
+
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+    }
+
+
+}
